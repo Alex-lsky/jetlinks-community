3425715900adadf54d2666d6960fca3a2f0634e2
柳皓天 <sky_liuhaotian@163.com>
2023-06-30 13:18:29 +0800

    同步更新

--- jetlinks-standalone/docker-entrypoint.sh
+++ jetlinks-standalone/docker-entrypoint.sh
@@ -3,6 +3,6 @@ java $JAVA_OPTS -server \
 -XX:+UnlockExperimentalVMOptions \
 -XX:+UseCGroupMemoryLimitForHeap \
 -XX:-OmitStackTraceInFastThrow \
--Xdebug -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=8844 \
+-Xdebug -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5005 \
 -Djava.security.egd=file:/dev/./urandom \
 org.springframework.boot.loader.JarLauncher
